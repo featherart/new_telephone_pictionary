42c3fb4148c2098cb49c15a0accbd97c39891e70
Feather <lisafeather@gmail.com>
2013-12-10 14:24:36 -0800

    all Devise removed, omniauth added, need to tie turns to sessions so we know where we are at in game

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+  def create
+  	user = User.from_omniauth(env["omniauth.auth"])
+  	session[:user_id] = user.id
+  	redirect_to root_url
+
+  	# add the turn here once Facebook auth working
+  	# @turn = Turn.create(turn_number: 1, user_id: user.id)
+  end
+
+  def destroy
+  	session[:user_id] = nil
+  	redirect_to root_url
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 TelephonePictionary::Application.routes.draw do
   
+  get "sessions/create"
+
+  get "sessions/destroy"
+
+  get "sessions_controller/create"
+
+  get "sessions_controller/destroy"
+
   #match 'auth/:provider/callback', to: 'sessions#create'
   root to: "storylines#index"
 
