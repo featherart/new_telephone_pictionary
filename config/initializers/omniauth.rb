e30d49ee249d7af90c9b45995b904b05f012caf6
Feather <lisafeather@gmail.com>
2013-12-10 16:49:15 -0800

    oops

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  #provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
-  provider :facebook, '231758050606', '1e8fb7a07d232c9296e268e55f3901f3'
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
 end
