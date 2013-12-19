00040e79f99c126863bc50e04ab2777aae55b7b4
Feather <lisafeather@gmail.com>
2013-12-18 19:55:15 -0800

    add Twilio methods, need to add a view to add users to a game

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :oauth_expires_at, :oauth_token, :provider, :uid
-  
+  attr_accessible :name, :oauth_expires_at, :oauth_token, :provider, :uid 
   has_one :turn
+
   def self.from_omniauth(auth)
     where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
       user.provider = auth.provider
@@ -12,5 +12,19 @@ class User < ActiveRecord::Base
       user.save!
     end
   end
+
+  def send_text(text)
+    account_sid    = ENV["TWILIO_ACCOUNT_SID"]
+    auth_token     = ENV["TWILIO_AUTH_TOKEN"]
+    client = Twilio::REST::Client.new account_sid, auth_token
+
+    account = client.account
+    message = account.sms.messages.create({
+      :from => ENV["phone"],
+      :to => self.phone,
+      :body => text})
+    puts message
+  end
+
 
 end
